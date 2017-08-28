f197eeb596f24887e58cb95cedc44d09fe5a19b5
Administrator <592759374@qq.com>
2017-08-28 19:07:21 +0800

    删除一些东西

--- XiongMao_project/app/src/main/java/com/jiyun/qcloud/dashixummoban/activity/vitamio/VitamioActivity.java
+++ XiongMao_project/app/src/main/java/com/jiyun/qcloud/dashixummoban/activity/vitamio/VitamioActivity.java
@@ -20,7 +20,7 @@ public class VitamioActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);//
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_vitamio);
 //        initView();
 //        initVideoSettings();
