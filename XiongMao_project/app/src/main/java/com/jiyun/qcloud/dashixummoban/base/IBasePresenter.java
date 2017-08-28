75648df9e730d92e627e74f69a13f8166c40eee6
Administrator <592759374@qq.com>
2017-08-28 19:27:50 +0800

    shanche

--- XiongMao_project/app/src/main/java/com/jiyun/qcloud/dashixummoban/base/IBasePresenter.java
+++
@@ -1,9 +0,0 @@
-package com.jiyun.qcloud.dashixummoban.base;
-
-/**
- * Created by xingge on 2017/7/11.
- */
-
-public interface IBasePresenter {
-    void start();
-}
--- XiongMao_project/app/src/main/java/com/jiyun/qcloud/dashixummoban/ui/a.java
+++
@@ -1,9 +0,0 @@
-package com.jiyun.qcloud.dashixummoban.ui;
-
-/**
- * Created by chj on 2017/8/20.
- */
-
-public class a {
-
-}
