cb06ea942c9dc90a7bcd2755e17227bf676fd645
Administrator <592759374@qq.com>
2017-08-28 16:02:45 +0800

    杜智宏提交

---
+++ XiongMao_project/app/src/main/java/com/jiyun/qcloud/dashixummoban/activity/denglu/shezhi/SheZhiActivity.java
@@ -0,0 +1,15 @@
+package com.jiyun.qcloud.dashixummoban.activity.denglu.shezhi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jiyun.qcloud.dashixummoban.R;
+
+public class SheZhiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_she_zhi);
+    }
+}
